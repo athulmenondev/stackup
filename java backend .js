d61bc45cbe999ff00099d13b2168cea8930cccbe
shahwan-k <shahwankdev@gmail.com>
2023-11-09 20:14:10 +0530

    initial commit

---
+++ java backend .js
@@ -0,0 +1,41 @@
+from flask import Flask, request, render_template, redirect, url_for
+
+app = Flask(__name__)
+
+# Simulated database for products
+products = [
+    {"id": 1, "name": "Product 1", "price": 10.00},
+    {"id": 2, "name": "Product 2", "price": 20.00},
+    # Add more products here
+]
+
+# Simulated shopping cart (in-memory storage)
+cart = []
+
+@app.route('/')
+def home():
+    return render_template('index.html', products=products)
+
+@app.route('/add_to_cart/<int:product_id>')
+def add_to_cart(product_id):
+    product = next((p for p in products if p["id"] == product_id), None)
+    if product:
+        cart.append(product)
+    return redirect(url_for('home'))
+
+    @app.route('/cart')
+    def view_cart():
+        total_price = sum(product["price"] for product in cart)
+        return render_template('cart.html', cart=cart, total_price=total_price)
+    
+    @app.route('/checkout')
+    def checkout():
+        # You can add payment processing logic here
+        cart.clear()
+        return "Checkout Successful!"
+    
+    if __name__ == '__main__':
+        app.run(debug=True)
+    
+    
+    
---
+++ jimp.py
@@ -0,0 +1,44 @@
+from flask import Flask, request, jsonify
+from PIL import Image
+from io import BytesIO
+
+app = Flask(__name__)
+
+# Dummy in-memory database
+products = []
+
+# Route to get all products
+@app.route('/products', methods=['GET'])
+def get_products():
+    return jsonify(products)
+
+# Route to add a new product
+@app.route('/products', methods=['POST'])
+def add_product():
+    try:
+        data = request.get_json()
+        name = data['name']
+        price = data['price']
+        image_url = data['image_url']
+
+        # Process and save the image using Pillow
+        image = Image.open(BytesIO(image_url))
+        image.thumbnail((200, 200))  # Resize the image
+        image_buffer = BytesIO()
+        image.save(image_buffer, format="JPEG")
+
+        product = {
+            'name': name,
+            'price': price,
+            'image_url': image_buffer.getvalue(),
+        }
+
+        products.append(product)
+
+        return jsonify(product), 201
+
+    except Exception as e:
+        return jsonify({'error': 'Internal Server Error'}), 500
+
+if __name__ == '__main__':
+    app.run(debug=True)
